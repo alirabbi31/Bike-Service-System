6e2d0ddf533dd873abcd928f113343e61fe8c01a
Softzino <rabbi@softzino.com>
2025-05-17 11:07:27 +0600

    Init: project setup with vue inertia

---
+++ database/migrations/2025_05_17_050015_create_parts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('parts', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 100);
+            $table->string('name', 100);
+            $table->string('stock', 100);
+            $table->string('dealer_price');
+            $table->string('retail_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('parts');
+    }
+};
